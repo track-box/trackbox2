663b01ec482d544d2602e6aa93e0d6eb5e17ae90
tattii <yuta.tatti@gmail.com>
2016-11-19 17:43:02 +0900

    add body parser urlencoded

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var multer = require('multer');
 var corser = require("corser");
 app.use(corser.create());
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({ limit:'5mb' }));
 app.use(multer());
 
